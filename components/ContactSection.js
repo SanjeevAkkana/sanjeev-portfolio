245148c7d11547fcb71e616f82e4a47e84cbea02
SanjeevAkkana <asanjeevroyal@gmail.com>
2024-12-18 22:03:09 +0530

    Commit

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -23,6 +23,9 @@ const ContactSection = () => {
               <span className="text-green-400 font-mono">&gt; Full Name:</span> Akkana Sanjeev
             </p>
             <p>
+              <span className="text-green-400 font-mono">&gt; Phone Number:</span> +91 7075972027
+            </p>
+            <p>
               <span className="text-green-400 font-mono">&gt; Role:</span> Full-Stack Developer
             </p>
             <p>
@@ -38,66 +41,6 @@ const ContactSection = () => {
               </a>
             </p>
           </div>
-        </div>
-
-        {/* Contact Form */}
-        <div className="lg:w-1/2 space-y-6 mt-8 lg:mt-0">
-          <p className="border-l-4 border-green-400 pl-2 font-mono font-semibold text-xl">
-            Send me a message
-          </p>
-          <form
-            action="mailto:akkanasanjeev@gmail.com"
-            method="post"
-            enctype="text/plain"
-            className="space-y-4"
-          >
-            <div>
-              <label htmlFor="name" className="font-mono">
-                Your Name
-              </label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                required
-                placeholder="Your Name"
-                className="w-full p-3 mt-2 bg-gray-900 border border-gray-700 rounded text-white focus:outline-none"
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="font-mono">
-                Your Email
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                placeholder="Your Email"
-                className="w-full p-3 mt-2 bg-gray-900 border border-gray-700 rounded text-white focus:outline-none"
-              />
-            </div>
-            <div>
-              <label htmlFor="message" className="font-mono">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                required
-                placeholder="Your Message"
-                rows="5"
-                className="w-full p-3 mt-2 bg-gray-900 border border-gray-700 rounded text-white focus:outline-none"
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="mt-6 w-full py-3 bg-green-500 hover:bg-green-600 text-white font-mono font-semibold rounded"
-            >
-              Send Email
-            </button>
-          </form>
         </div>
       </div>
     </section>
